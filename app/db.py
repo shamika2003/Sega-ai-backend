fbccf31597a3927414f8ed2b35da03850e209886
Shamika Achinthya <152179753+shamika2003@users.noreply.github.com>
2026-02-15 21:29:54 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -5,11 +5,11 @@ import os
 import uuid
 
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Shamika@2003")
-DB_NAME = os.getenv("DB_NAME", "segaai")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 pool = None  # global pool
 
